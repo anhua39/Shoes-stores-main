41f45716eef907e8092a1c445f69b8d939789fb3
anhua39 <116139878+anhua39@users.noreply.github.com>
2023-06-23 08:10:46 +0700

    sds
    as

--- resources/views/frontend/pages/pages/aboutUs.blade.php
+++ resources/views/frontend/pages/pages/aboutUs.blade.php
@@ -65,7 +65,7 @@
                         <p>“Going out after work? Take your butane curling iron with you to the office, heat it up,
                             style your hair before you leave the office and you won’t have to make a trip back home.”
                         </p>
-                        <div class="testimonial__author">
+                        <!-- <div class="testimonial__author">
                             <div class="testimonial__author__pic">
                                 <img src="img/about/testimonial-author.jpg" alt="">
                             </div>
@@ -73,7 +73,7 @@
                                 <h5>Augusta Schultz</h5>
                                 <p>Fashion Design</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
                 <div class="col-lg-6 p-0">
